c85208ec9ef546100c19a6eea1dd7480902c6baf
Kyle Rand <krand7@gmail.com>
2014-09-16 16:18:32 -0400

    Altered navigation setup, allows for preloaded movements

---
+++ SlowMoVideo/KPRChooseMoveTableViewController.h
@@ -0,0 +1,12 @@
+//
+//  KPRChooseMoveTableViewController.h
+//  SlowMoVideo
+//
+//  Created by Kyle Rand on 9/16/14.
+//  Copyright (c) 2014 PracticeCoding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KPRChooseMoveTableViewController : UITableViewController 
+@end
